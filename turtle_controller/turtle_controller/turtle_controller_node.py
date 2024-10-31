244eb509c19244db5ae4c4bf973ee6a63b01acec
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-10-31 23:10:41 +0000

    working move turtle right

--- turtle_controller/turtle_controller/turtle_controller_node.py
+++ turtle_controller/turtle_controller/turtle_controller_node.py
@@ -1,20 +1,20 @@
 
 import rclpy
 from rclpy.node import Node
-from turtlesim.msg import Pose
+from geometry_msgs.msg import Twist
 
 class TurtleController(Node):
     def __init__(self) -> None:
         super().__init__('TurtleController')
         self.get_logger().info("node initlized")
-        self._turtle_publisher = self.create_publisher(Pose,"/turtle1/pose",10)
+        self._turtle_publisher = self.create_publisher(Twist,"/turtle1/cmd_vel",10)
         self._timer = self.create_timer(0.1,self._timer_callback)
     
     def _timer_callback(self) -> None:
-        output_pose = Pose()
-        output_pose.x = 1.0
-        output_pose.y = 2.0
-        
+        output_pose = Twist()
+        output_pose.linear.x = 5.0
+        output_pose.angular.y = 3.14
+        self.get_logger().info(f"published {output_pose}")
         self._turtle_publisher.publish(output_pose)
         
         
@@ -22,14 +22,10 @@ class TurtleController(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-
     turtle_controller = TurtleController()
-
+    
     rclpy.spin(turtle_controller)
 
-    # Destroy the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
     turtle_controller.destroy_node()
     rclpy.shutdown()
 
