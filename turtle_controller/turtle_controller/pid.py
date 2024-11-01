ffa89e4aadd0baf0cbc269972d9e40b9720e7844
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 06:19:41 +0000

    refactor pid

--- turtle_controller/turtle_controller/pid.py
+++ turtle_controller/turtle_controller/pid.py
@@ -1,8 +1,6 @@
 def pid(target: float, current: float, max_error: float, p: float):
     error = target - current
     output_pid = error / max_error
-    if output_pid >= 1:
-        return p
-    if output_pid <= -1:
-        return -p
+    output_pid = min(output_pid, 1)
+    output_pid = max(output_pid, -1)
     return output_pid * p
