a4744a00676b19468dde2b6624fcd26a738e7f90
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 07:26:55 +0000

    finish pi controller

--- turtle_controller/test/test_pid.py
+++ turtle_controller/test/test_pid.py
@@ -16,6 +16,22 @@ def test_when_error_leser_than_negative_max_error_then_pid_returns_minus_p():
     assert pid.calculate_output(-15) == -2
 
 
-def test_when_error_half_of_max_error_then_pid_returns_half_p():
+def test_when_error_half_then_pid_returns_half_p():
     pid = Pid(p=2)
     assert pid.calculate_output(0.5) == 1
+
+
+def test_when_i_is_1_and_error_is_1_then_do_half_error():
+    p = 2
+    i = 5
+    result = 4.5
+    pid = Pid(p=p, i=i)
+    assert pid.calculate_output(1) == result
+
+
+def test_when_i_is_1_and_error_is_1_and_sample_is_0_5_then_do_half_error():
+    p = 2
+    i = 5
+    result = 3.25
+    pid = Pid(p=p, i=i, sample_time=0.5)
+    assert pid.calculate_output(1) == result
--- turtle_controller/turtle_controller/pid.py
+++ turtle_controller/turtle_controller/pid.py
@@ -1,10 +1,18 @@
 class Pid:
-    def __init__(self, p: float) -> None:
+    def __init__(self, p: float, i: float = 0, sample_time: float = 1) -> None:
         self._p = p
+        self._i = i
+        self._sample_time = sample_time
+        self._prev_error = 0
 
-    def calculate_output(self, error: float):
-        error = min(error, 1)
-        error = max(error, -1)
+    def calculate_output(self, error: float) -> float:
+        error = self._range_between_1_and_minus_1(error)
         output_pid = 0
         output_pid += error * self._p
+        output_pid += self._i * (error - self._prev_error) * self._sample_time / 2
         return output_pid
+
+    def _range_between_1_and_minus_1(self, error: float) -> float:
+        error = min(error, 1)
+        error = max(error, -1)
+        return error
