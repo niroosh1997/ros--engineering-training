2d4056526f34bd520fefd905aacc9b87bda84581
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 06:14:59 +0000

    working p

--- turtle_controller/test/test_pid.py
+++ turtle_controller/test/test_pid.py
@@ -2,4 +2,14 @@ from turtle_controller.pid import pid
 
 
 def test_when_error_is_zero_then_pid_returns_zero():
-    pid(target=5, current=5, p=10)
+    assert pid(target=5, current=5, max_error=20, p=10) == 0
+
+
+def test_when_error_bigger_than_max_error_then_pid_returns_p():
+    p = 10
+    assert pid(target=7, current=5, max_error=2, p=p) == p
+
+
+def test_when_error_half_of_max_error_then_pid_returns_half_p():
+    p = 10
+    assert pid(target=6, current=5, max_error=2, p=p) == p / 2
--- turtle_controller/turtle_controller/pid.py
+++ turtle_controller/turtle_controller/pid.py
@@ -1,2 +1,6 @@
-def pid(target: float, current: float, p: float):
-    return target - current
+def pid(target: float, current: float, max_error: float, p: float):
+    error = target - current
+    output_pid = error / max_error
+    if output_pid >= 1:
+        return p
+    return output_pid * p
