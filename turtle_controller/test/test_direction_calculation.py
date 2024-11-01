5a44328ca91bf6438d5f9c8ae7387a839805f1cf
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 05:23:54 +0000

    finished function of calculate theta

--- turtle_controller/test/test_direction_calculation.py
+++ turtle_controller/test/test_direction_calculation.py
@@ -19,3 +19,19 @@ def test_when_deg_in_forth_square_then_taget_theta_is_like_calculation():
     taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
     assert round(taget_theta,4) == round(0.166666667 * math.pi,4)
 
+def test_when_deg_in_second_square_then_taget_theta_is_like_calculation():
+    currect_x = 0.0
+    currect_y = 0.0
+    target_x = -1.0
+    target_y = math.sqrt(3)
+    taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
+    assert round(taget_theta,4) == round(-0.8333333 * math.pi,4)
+
+def test_when_deg_in_third_square_then_taget_theta_is_like_calculation():
+    currect_x = 0.0
+    currect_y = 0.0
+    target_x = -1.0
+    target_y = -math.sqrt(3)
+    taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
+    assert round(taget_theta,4) == round(-0.166666667 * math.pi,4)
+
--- turtle_controller/turtle_controller/direction_calculation.py
+++ turtle_controller/turtle_controller/direction_calculation.py
@@ -3,7 +3,7 @@ import math
 def calculate_target_theta(current_x: float, current_y: float, target_x: float, target_y: float):
     tan_target = target_y / target_x
     x_axis_angle = 0.5 * math.pi
-    if target_y >= 0:
+    if target_x >= 0:
         return x_axis_angle + math.atan(tan_target) 
-    if target_y < 0:
-        return x_axis_angle + math.atan(tan_target) 
+    if target_x < 0:
+        return (-x_axis_angle + math.atan(tan_target))
