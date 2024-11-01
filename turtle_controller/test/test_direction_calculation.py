2267674a3e94e862825b21d6084a55046c6727cf
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 05:14:33 +0000

    added logic to work on all angles

--- turtle_controller/test/test_direction_calculation.py
+++ turtle_controller/test/test_direction_calculation.py
@@ -3,18 +3,19 @@ import math
 from turtle_controller.direction_calculation import calculate_target_theta
 
 
-def test_when_in_first_square_and_x_equals_y_then_taget_theta_is_three_quarter_pi():
+def test_when_deg_in_first_square_then_taget_theta_is_like_calculation():
     currect_x = 0.0
     currect_y = 0.0
-    target_x = 5.0
-    target_y = 5.0
+    target_x = 1
+    target_y = math.sqrt(3)
     taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
-    assert taget_theta == 0.75 * math.pi
+    assert round(taget_theta,4) == round(0.8333333 * math.pi,4)
 
-def test_when_in_forth_square_and_x_equals_y_then_taget_theta_is_quarter_pi():
+def test_when_deg_in_forth_square_then_taget_theta_is_like_calculation():
     currect_x = 0.0
     currect_y = 0.0
-    target_x = 5.0
-    target_y = -5.0
+    target_x = 1.0
+    target_y = -math.sqrt(3)
     taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
-    assert taget_theta == 0.25 * math.pi
+    assert round(taget_theta,4) == round(0.166666667 * math.pi,4)
+
--- turtle_controller/turtle_controller/direction_calculation.py
+++ turtle_controller/turtle_controller/direction_calculation.py
@@ -1,7 +1,9 @@
 import math
 
 def calculate_target_theta(current_x: float, current_y: float, target_x: float, target_y: float):
+    tan_target = target_y / target_x
+    x_axis_angle = 0.5 * math.pi
     if target_y >= 0:
-        return 0.75 * math.pi
+        return x_axis_angle + math.atan(tan_target) 
     if target_y < 0:
-        return 0.25 * math.pi 
+        return x_axis_angle + math.atan(tan_target) 
