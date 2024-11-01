9b275f1aeaa05278c463fc2855e6f608a493b85a
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-01 00:05:58 +0000

    working test of one example

--- turtle_controller/test/test_direction_calculation.py
+++ turtle_controller/test/test_direction_calculation.py
@@ -1,3 +1,12 @@
+import math
+
 from turtle_controller.direction_calculation import calculate_target_theta
+
+
 def test_example():
-    calculate_target_theta()
+    currect_x = 0.0
+    currect_y = 0.0
+    target_x = 5.0
+    target_y = 5.0
+    taget_theta = calculate_target_theta(currect_x, currect_y, target_x, target_y)
+    assert taget_theta == 0.75 * math.pi
--- turtle_controller/turtle_controller/direction_calculation.py
+++ turtle_controller/turtle_controller/direction_calculation.py
@@ -1,2 +1,4 @@
-def calculate_target_theta():
-    position_turtle = 5
+import math
+
+def calculate_target_theta(currect_x: float, currect_y: float, target_x: float, target_y: float):
+    return 0.75 * math.pi
