6472f21c04290f69d545f10a262374810161b2ec
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-02 15:36:36 +0200

    change files of utils, still import doesnt works

--- turtle_controller/turtle_controller/image_vision/ball_finding.py
+++ turtle_controller/turtle_controller/image_vision/ball_finding.py
@@ -1,11 +1,26 @@
-import numpy
 import cv2
+from turtle_controller.image_vision.shape_utils import get_center_of_contour_shape
 
 
 def main():
+
     real_image = cv2.imread("photo_with_ball.jpg")
-    gray_image = cv2.imread("photo_with_ball.jpg", 0)
-    cv2.imshow("result", gray_image)
+    gray_image = cv2.imread("photo_with_ball.jpg", cv2.IMREAD_GRAYSCALE)
+    down_width = 640
+    down_height = 480
+    down_points = (down_width, down_height)
+    real_image = cv2.resize(real_image, down_points, interpolation=cv2.INTER_LINEAR)
+    real_image1 = cv2.resize(real_image, down_points, interpolation=cv2.INTER_LINEAR)
+    gray_image = cv2.resize(gray_image, down_points, interpolation=cv2.INTER_LINEAR)
+
+    _, thresh = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
+    contours, h = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contour_with_biggest_area = max(contours, key=cv2.contourArea)
+    center = get_center_of_contour_shape(contour_with_biggest_area)
+
+    cv2.circle(real_image, center, 2, (0, 0, 255), -1)
+
+    cv2.imshow("result", real_image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
---
+++ turtle_controller/turtle_controller/image_vision/shape_utils.py
@@ -0,0 +1,24 @@
+from cv2.typing import Moments, MatLike
+import cv2
+
+
+def get_moment_sum_of_x(moments: Moments) -> float:
+    return moments["m10"]
+
+
+def get_moment_sum_of_y(moments: Moments) -> float:
+    return moments["m01"]
+
+
+def get_moments_length(moments: Moments) -> float:
+    return moments["m00"]
+
+
+def get_center_of_contour_shape(shape: MatLike) -> tuple[int, int]:
+    moments_of_shape = cv2.moments(shape)
+    center = get_center_of_contour_shape(moments_of_shape)
+    center = (
+        int(get_moment_sum_of_x(moments_of_shape) / get_moments_length(moments_of_shape)),
+        int(get_moment_sum_of_y(moments_of_shape) / get_moments_length(moments_of_shape)),
+    )
+    return center
