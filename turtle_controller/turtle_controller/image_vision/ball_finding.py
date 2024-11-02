eaac06c924e5ef282f6058fe29ab5b4798b799a7
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-02 16:53:08 +0200

    added ros image

--- turtle_controller/turtle_controller/image_vision/ball_finding.py
+++ turtle_controller/turtle_controller/image_vision/ball_finding.py
@@ -1,35 +1,19 @@
 import cv2
+from sensor_msgs.msg import Image
 from turtle_controller.image_vision.shape_utils import get_center_of_contour_shape
 import numpy as np
 
 
-def main():
-
-    real_image = cv2.imread("turtle_controller/turtle_controller/image_vision/photo_with_ball.jpg")
-    gray_image = cv2.imread(
-        "turtle_controller/turtle_controller/image_vision/photo_with_ball.jpg",
-        cv2.IMREAD_GRAYSCALE,
-    )
+def center_of_ball(image: Image) -> tuple[int, int]:
     down_width = 640
     down_height = 480
     down_points = (down_width, down_height)
 
-    real_image = cv2.resize(real_image, down_points, interpolation=cv2.INTER_LINEAR)
-    real_image1 = cv2.resize(real_image, down_points, interpolation=cv2.INTER_LINEAR)
-    gray_image = cv2.resize(gray_image, down_points, interpolation=cv2.INTER_LINEAR)
+    resized_image = cv2.resize(image.data, down_points, interpolation=cv2.INTER_LINEAR)
+    gray_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
 
     _, thresh = cv2.threshold(gray_image, 127, 255, cv2.THRESH_BINARY)
-    contours, h = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     contour_with_biggest_area = max(contours, key=cv2.contourArea)
 
     center = get_center_of_contour_shape(contour_with_biggest_area)
-
-    cv2.circle(real_image, center, 2, (0, 0, 255), -1)
-
-    cv2.imshow("result", real_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-if __name__ == "__main__":
-    main()
