5704bdc0daf949b60479a9bcd4b4e289584c051a
Nir Blagovsky <nir.blagovsky@mapcore.onmicrosoft.com>
2024-11-02 16:52:35 +0200

    subscribe to image

--- turtle_controller/turtle_controller/turtle_controller_node.py
+++ turtle_controller/turtle_controller/turtle_controller_node.py
@@ -2,7 +2,7 @@ import rclpy
 from geometry_msgs.msg import Twist
 from turtlesim.msg import Pose
 from rclpy.node import Node
-
+from sensor_msgs.msg import Image
 from turtle_controller.utils.specific_pid import MovmentPid
 
 
@@ -11,13 +11,12 @@ class TurtleController(Node):
         super().__init__("TurtleController")
         self.get_logger().info("node initlized")
         self._turtle_publisher = self.create_publisher(Twist, "/turtle1/cmd_vel", 10)
-        self._turtle_subscriber = self.create_subscription(
-            Pose, "/turtle1/pose", self._handle_turtle_pos, 10
-        )
-        self._turtle_subscriber = self.create_subscription(
-            Pose, "/turtle_controller/pose", self._handle_go_to_pos, 10
-        )
+        self.create_subscription(Pose, "/turtle1/pose", self._handle_turtle_pos, 10)
+        self.create_subscription(Pose, "/turtle_controller/pose", self._handle_go_to_pos, 10)
+        self.create_subscription(Image, "/camera/image_raw", self._handle_image)
+
         self._timer = self.create_timer(0.1, self._timer_callback)
+
         self._movement_pid = MovmentPid(p=10, i=0.2, max_error=10, sample_time=0.1)
         self._current_pos = Pose()
         self._go_to_pos = Pose()
@@ -36,6 +35,9 @@ class TurtleController(Node):
 
     def _handle_go_to_pos(self, turtle_pos: Pose):
         self._go_to_pos = turtle_pos
+
+    def _handle_image(image: Image):
+        pass
 
 
 def main(args=None):
